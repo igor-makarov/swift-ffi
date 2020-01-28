ff887dfbca0e3c870ae198d55c4cc83964841176
Igor Makarov <igormaka@gmail.com>
2020-01-28 12:06:54 +0200

    add some code comments

--- Sources/swift-ffi/swift_ffi.swift
+++ Sources/swift-ffi/swift_ffi.swift
@@ -1,19 +1,27 @@
 import Foundation
 
+// prints the provided C String
 @_cdecl("swift_ffi_print")
 public func FFIPrint(_ cString: UnsafePointer<CChar>) {
+    // Convert from C String to Swift string
     let converted = String(cString: cString)
     print("Swift print: \"\(converted)\"")
 }
 
+// multiplies two integers
 @_cdecl("swift_ffi_values_multiply") 
 public func FFIValuesMultiply(_ argument1: Int, _ argument2: Int) -> Int{
     argument1 * argument2
 }
 
+// duplicates the provided C String
+// returns a 'fresh' string (API consumer is responsible to free it)
 @_cdecl("swift_ffi_duplicate_string")
 public func FFIDuplicate(_ cString: UnsafePointer<CChar>) -> UnsafePointer<CChar> {
+    // Convert from C String to Swift string
     let converted = String(cString: cString)
     let duplicated = converted + " " + converted
+    // use libc `strdup` to return a retained C String
+    // freeing the string is the responsiblity of the API consumer
     return UnsafePointer(strdup(duplicated))
 }
--- run.rb
+++ run.rb
@@ -2,15 +2,21 @@ require 'ffi'
 
 module SwiftFFILib
   extend FFI::Library
+  # Two possible extensions, .so for Linux, .dylib for macOS
   ffi_lib ['.build/debug/libswift-ffi.dylib', '.build/debug/libswift-ffi.so']
 
   attach_function :swift_ffi_print, [:string], :void
   attach_function :swift_ffi_values_multiply, [:int, :int], :int
+  # attach with a temporary name - wrapped below  
   attach_function :unsafe_swift_ffi_duplicate_string, :swift_ffi_duplicate_string, [:string], :strptr
 
-  def self.swift_ffi_duplicate_string(string) 
+  # the wrapped version of the C function 
+  def self.swift_ffi_duplicate_string(string)
+    # get the string with a pointer
     result, ptr = unsafe_swift_ffi_duplicate_string(string)
+    # create a managed copy
     result = String.new(result)
+    # free the pointer
     free(ptr)
     result
   end
@@ -21,11 +27,19 @@ module SwiftFFILib
 end
 
 string_value = "String Value"
+
+# print using Swift `print(_:)`
 STDERR.puts "Calling a func that prints \"#{string_value}\""
 SwiftFFILib.swift_ffi_print(string_value)
+STDERR.puts
+
+# duplicate a string using the wrapped function
 STDERR.puts "Calling a func that duplicates \"#{string_value}\""
 duplicated = SwiftFFILib.swift_ffi_duplicate_string(string_value)
 STDERR.puts "Duplicated = \"#{duplicated}\""
+STDERR.puts
+
+# multiply two integers
 multiplied = SwiftFFILib.swift_ffi_values_multiply(2, 3)
 STDERR.puts "Calling a func with Int arguments"
 STDERR.puts "2 * 3 = #{multiplied}"
